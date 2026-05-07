41820ab66c13c1b59379d31d34d3de336bfd2b45
Darenlof <6vrxxjchkp@privaterelay.appleid.com>
2026-05-07 15:27:14 +0200

    Update tag.js

--- commands/tag.js
+++ commands/tag.js
@@ -12,7 +12,7 @@ export async function tagall(client, message) {
         const text = participants.map(user => `@${user.split('@')[0]}`).join(' \n')
 
         await client.sendMessage(remoteJid, {
-            text: `╭─⌈ 🚀 Digital Crew Broadcast ⌋\n│\n${text}\n│\n╰─⌊ Powered by DC243 ⌉`,
+            text: `╭─⌈ 🚀 Daren Broadcast ⌋\n│\n${text}\n│\n╰─⌊ Powered by DC243 ⌉`,
             mentions: participants
         })
 
